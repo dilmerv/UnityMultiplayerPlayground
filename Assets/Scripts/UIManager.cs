7c0f5ad6494453d5f866b4ebf575101d5f3ccdbc
Dilmer Valecillos <dilmervst@gmail.com>
2021-11-09 07:06:35 -0700

    Added UIManager and network manager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button startServerButton;
+
+    [SerializeField]
+    private Button startHostButton;
+
+    [SerializeField]
+    private Button startClientButton;
+
+    void Start()
+    {
+        startServerButton.onClick.AddListener(() =>
+        {
+            if (NetworkManager.Singleton.StartServer())
+                Logger.Instance.LogInfo("Server started...");
+            else
+                Logger.Instance.LogInfo("Unable to start server...");
+        });
+
+        startHostButton.onClick.AddListener(() =>
+        {
+            if(NetworkManager.Singleton.StartHost())
+                Logger.Instance.LogInfo("Host started...");
+            else
+                Logger.Instance.LogInfo("Unable to start host...");
+        });
+
+        startClientButton.onClick.AddListener(() =>
+        {
+            if(NetworkManager.Singleton.StartClient())
+                Logger.Instance.LogInfo("Client started...");
+            else
+                Logger.Instance.LogInfo("Unable to start client...");
+        });
+    }
+}
