2e77bbbff3c2c44593614eff5b0f3e5053f2d2e2
Dilmer Valecillos <dilmervst@gmail.com>
2021-11-14 16:21:19 -0700

    Small fix for player Hud

--- Assets/Scripts/PlayerHud.cs
+++ Assets/Scripts/PlayerHud.cs
@@ -7,14 +7,27 @@ public class PlayerHud : NetworkBehaviour
     [SerializeField]
     private NetworkVariable<NetworkString> playerNetworkName = new NetworkVariable<NetworkString>();
 
+    private bool overlaySet = false;
+
     public override void OnNetworkSpawn()
     {
         if(IsServer)
         {
             playerNetworkName.Value = $"Player {OwnerClientId}";
         }
+    }
 
+    public void SetOverlay()
+    {
         var localPlayerOverlay = gameObject.GetComponentInChildren<TextMeshProUGUI>();
         localPlayerOverlay.text = $"{playerNetworkName.Value}";
+    }
+
+    public void Update()
+    {
+        if(!overlaySet && !string.IsNullOrEmpty(playerNetworkName.Value))
+        {
+            SetOverlay();
+        }
     }
 }
